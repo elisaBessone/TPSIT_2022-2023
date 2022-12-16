0e40100a815f8680ba8ab09d9616f85be5272c6e
Elisa Bessone <elisa.bessone@itiscuneo.eu>
2022-12-16 10:06:01 +0100

    Compito thread --> tabellina.

---
+++ 007_thread/tabellina/client_tabellina.py
@@ -0,0 +1,35 @@
+from socket import socket, AF_INET, SOCK_STREAM
+import threading
+import time
+
+
+
+def main():
+    client = socket(AF_INET, SOCK_STREAM)
+    client.connect(("127.0.0.1", 8000))
+
+    while True:
+        #chiedo di che numero l'utente vuole fare la tabellina.
+        operazione = input("Di quale numero vuoi fare la tabellina? (exit per uscire)>>")
+        client.sendall(operazione.encode("utf-8"))
+
+        #se invia il comando exit finisce la connessione con il server
+        if operazione.upper() == "EXIT":
+            print("Chiusura connessione con il server...")
+            client.close()
+            break
+        #riceve per 11 volte il numero del calcolo della tabellina
+        for i in range(0,11):
+            #ricevo il messaggio
+            risposta = client.recv(4096)
+            
+            #se mi arriva errore chiudo la connessione con il server
+            if risposta.decode("utf-8").upper() == "ERROR":
+                print("Error...")
+                break 
+            else:
+                print(risposta.decode("utf-8"))
+    
+
+if __name__ == "__main__":
+    main()
---
+++ 007_thread/tabellina/server_tabellina.py
@@ -0,0 +1,42 @@
+from socket import socket, AF_INET, SOCK_STREAM
+import threading
+import time
+
+def server_tabellina(conn, addr):
+    while conn:
+        msg = conn.recv(4096).decode('utf-8')
+        #se ricevo exit chiudo la connessione con quel client
+        if msg.upper() == "EXIT":
+            print(f"Chiusura connessione con {addr}")
+            conn.send(msg.encode('utf-8'))
+            conn.close()
+            break
+        else:
+            try:
+                num_tabellina = int(msg)
+                print(f"Eseguo la tabellina del {num_tabellina}...")
+                #per 11 volte ciclo e invio il calcolo della tabellina
+                for i in range(0,11):
+                    ris = num_tabellina*i
+                    print(f" {num_tabellina} * {i} = {ris}")
+                    conn.sendall(str(ris).encode("utf-8"))
+                    time.sleep(2)
+            except:
+                print("Error")
+                conn.send("Error".encode("utf-8"))
+
+def main():
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(("127.0.0.1", 8000))
+    s.listen()
+
+    while True:
+        conn, addr = s.accept()
+        print(f"Connesso con {addr}.")
+
+        t = threading.Thread(target=server_tabellina, args=(conn, addr))
+        t.start()
+
+        
+if __name__ == "__main__":
+    main()
